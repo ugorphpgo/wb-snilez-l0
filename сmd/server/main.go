e75a11cefcaeeaf946ac6d935a3c919fa09ffdd2
ugorphpgo <ugorphpgo@gmail.com>
2025-09-23 13:57:27 +0300

    test insert and models

---
+++ сmd/server/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"wb-snilez-l0/internal/app"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	dbuser := "postgres"
+	dbpassword := "1234"
+	dbname := "order_db"
+
+	myApp, err := app.NewApp(fmt.Sprintf("postgres://%s:%s@db:5432/%s", dbuser, dbpassword, dbname))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer myApp.Close()
+
+	router := mux.NewRouter()
+
+	router.HandleFunc("/", myApp.MainHandler)
+
+	router.HandleFunc("/api/add", myApp.InsertHandler)
+
+	log.Println("Starting server on port 8080")
+	http.ListenAndServe(":8080", router)
+}
