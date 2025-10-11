5bf3c2953101fce77c5832c98d9ba54699fa98c0
ugorphpgo <ugorphpgo@gmail.com>
2025-10-11 18:13:10 +0300

    fix: update module path to full itub

--- scripts/kafka_producer/go.mod
+++ scripts/kafka_producer/go.mod
@@ -3,12 +3,15 @@ module order_producer
 go 1.25
 
 require (
+	github.com/ugorphpgo/wb-snilez-l0 v0.0.0-20251011142553-e75ad765df10
 	github.com/gorilla/mux v1.8.1
 	github.com/segmentio/kafka-go v0.4.48
-		)
+)
 
 require (
-	github.com/brianvoe/gofakeit/v7 v7.3.0 // indirect
+	github.com/brianvoe/gofakeit/v7 v7.7.3 // indirect
 	github.com/klauspost/compress v1.15.9 // indirect
 	github.com/pierrec/lz4/v4 v4.1.15 // indirect
 )
+
+replace github.com/ugorphpgo/wb-snilez-l0 => ../../
--- scripts/kafka_producer/producer.go
+++ scripts/kafka_producer/producer.go
@@ -50,3 +50,55 @@ func (tp *TestProducer) ProduceRandomOrder() error {
 func (tp *TestProducer) Close() {
 	tp.writer.Close()
 }
+
+func main() {
+	testKafka()
+
+	fmt.Println("Producer is up")
+
+	producer := MakeTestProducer()
+	defer producer.Close()
+
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "producer is ok")
+	})
+
+	r.HandleFunc("/producer", func(w http.ResponseWriter, r *http.Request) {
+		err := producer.ProduceRandomOrder()
+		if err != nil {
+			fmt.Fprintf(w, "Failed to produce random order: %v", err)
+		} else {
+			fmt.Fprintf(w, "Produced random order")
+		}
+	})
+	http.ListenAndServe(":8082", r)
+}
+
+func testKafka() {
+	conn, err := kafka.Dial("tcp", "localhost:9092")
+	if err != nil {
+		log.Printf("Failed to dial kafka: %s", err)
+	}
+	defer conn.Close()
+
+	topics, _ := conn.ReadPartitions()
+	found := false
+	for _, p := range topics {
+		if p.Topic == "orders" {
+			found = true
+			break
+		}
+	}
+	if !found {
+		err := conn.CreateTopics(kafka.TopicConfig{
+			Topic:             "orders",
+			NumPartitions:     1,
+			ReplicationFactor: 1,
+		})
+		if err != nil {
+			log.Fatalf("Failed to create topic: %s", err)
+		}
+	}
+}
